4e995949eef0bb1c5ec21f5f782510b01a091282
brindha0517 <brindhabrinz15@gmail.com>
2025-06-29 12:49:29 +0530

    Update Exercise_1_Setting_Up_JUnit.java

--- Week 2 - JUnit Tesing/main /java/Week 2 - JUnit Tesing/test /java/Exercise_1_Setting_Up_JUnit.java
+++ Week 2 - JUnit Tesing/main /java/Week 2 - JUnit Tesing/test /java/Exercise_1_Setting_Up_JUnit.java
@@ -1,1 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
+public class Exercise_1_Setting_Up_JUnit {
+
+    @Test
+    public void testAddition() {
+        int result = 2 + 3;
+        assertEquals(5, result);
+    }
+}
